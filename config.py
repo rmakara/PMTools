1a60f6c66a76000807ba26897e522ee2cbf4c74d
Rafal Makara <rafal.makara@gmail.com>
2017-03-11 12:10:25 +0100

    Change tabs to spaces

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 toggl = dict(
     token = "your_token",
-	workspace_id = 0,    
-	client_id = 0,
-	project_id = 0,
+    workspace_id = 0,    
+    client_id = 0,
+    project_id = 0,
 )
 
 jira = dict(
-	domain = 'https://your_domain.atlassian.net',
+    domain = 'https://your_domain.atlassian.net',
     username = 'your_username',
     project_key = 'your_project_key',
 )
