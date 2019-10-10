f03a5c2b62883c6e64272891cdbd188ae96c6fd9
Rafal Makara <rmakara@divante.co>
2019-10-10 16:43:46 +0200

    Fix RewriteTasksFromGithubToToggl

--- RewriteTasksFromGithubToToggl.py
+++ RewriteTasksFromGithubToToggl.py
@@ -9,13 +9,15 @@ def get_issues_from_github():
     github_repository_name = raw_input('Github repository name: ')
     github_repository_fullname = github_organization_name + '/' + github_repository_name
 
-    github_client = Github()
-    github_repo = github_client.get_repo(github_repository_fullname)
-    github_issue = github_repo.get_issues()
+    github_token = getpass.getpass()
+
+    github_client = Github(github_token)
+    github_repository = github_client.get_repo(github_repository_fullname)
+    github_issue = github_repository.get_issues()
 
     github_issues = {}
 
-    for issue in github_repo.get_issues():    
+    for issue in github_repository.get_issues():    
         github_issues[issue.number] = issue.title
 
     return github_issues
@@ -38,23 +40,24 @@ def add_issues_to_toggl(github_issues):
     for issue in toggl_issues_json:
         key_last_idx = issue['name'].find(" ")
         issue_key = issue['name'][:key_last_idx]
+        issue_key = int(issue_key.replace('#', '', 1))
         issue_summary = issue['name'][key_last_idx+1:]
         toggl_issues[issue_key] = [issue['id'], issue_summary]
 
     github_issues_count = len(github_issues.items())
 
-    for github_key, github_title in githubissues.items():
+    for github_key, github_title in github_issues.items():
         i += 1
         data = {
             'task': {}
         }
 
-        data['task']['name'] = "#" + github_key + " " + github_title
+        data['task']['name'] = "#" + str(github_key) + " " + github_title
         data['task']['pid'] = toggl_project_id
         data['task']['wid'] = config.toggl['workspace_id']
 
-        if github_key in toggl_issues:
-            if github_title != toggl_issues[github_key]:        
+        if int(github_key) in toggl_issues:
+            if "#" + str(github_key) + " " + github_title != toggl_issues[github_key]:        
                 updated_count += 1
                 print("Updating [{0}/{1}] {2} {3}.".decode('ascii', 'ignore').format(str(i), str(github_issues_count), github_key, github_title))
                 toggl.putRequest('https://www.toggl.com/api/v8/tasks/{0}'.format(toggl_issues[github_key][0]), data)
@@ -69,5 +72,5 @@ def add_issues_to_toggl(github_issues):
 
 ### main :)
 
-github_issues = get_issues_from_github())
+github_issues = get_issues_from_github()
 add_issues_to_toggl(github_issues)
